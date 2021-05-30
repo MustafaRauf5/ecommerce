8068dbf988984b59edb7395e094e3d8323cae611
MustafaRauf5 <mustafarauf5@gmail.com>
2021-05-31 03:56:03 +0500

    product display
    done displaying product on website

--- store/models.py
+++ store/models.py
@@ -15,12 +15,12 @@ class Customer(models.Model):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=False)
+    digital = models.BooleanField(default=False, null=True, blank=True)
     
-def __str__(self):
-          return self.name
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
