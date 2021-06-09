ee522b5ac517a3ae550eb3fe195f0e50f8892ad3
MustafaRauf5 <mustafarauf5@gmail.com>
2021-06-10 04:04:13 +0500

    js changes
    did some js changes

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import*
+from django.http import JsonResponse
+
+from .models import *
+
 
 # Create your views here.
 
@@ -16,21 +19,22 @@ def cart(request):
 		items = order.orderitem_set.all()
 	else:
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0,}
 
 	context = {'items':items, 'order':order}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-
-	
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
 	else:
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0,}
 
 	context = {'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)
