c25dc7aaa214f4b592178ea0c38da3b2e92092a9
MustafaRauf5 <mustafarauf5@gmail.com>
2021-06-16 04:43:21 +0500

    Js changes
    i have done changes js

--- static/Js/cart.js
+++ static/Js/cart.js
@@ -4,34 +4,32 @@ for(var i = 0; i < updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset
-        console.log('productId:', productId, 'action')
+        console.log('productId:', productId, 'action:', action,)
 
         console.log('USER:', user)
-        if(user === 'AnonymousUser'){
+        if(user == 'AnonymousUser'){
             console.log('Not logged in')
         }else{
-            console.log('User is logged in, sending data..')
+            updateUserOrder(productId, action)
         }
     }
     )
 }
 
 function updateUserOrder(productId, action){
-    console.log('User is logged in, sending data...')
+    console.log('User is logged in, sending da  ta...')
 
     var url = '/update_item/'
     
-    fetch(url, {
-        method:'POST',
-        headers:{
-            'Content-Type' : 'application/json',
-            'X-CSRFToken' : csrftoken,
-        },
-        body:JSON.stringify({'productId': productId, 'action':action})
-
-    })
-
-    .then((response) =>{
+        fetch(url, {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+                'X-CSRFToken':csrftoken,
+            },
+            body:JSON.stringify({'productId':productId, 'action':action})
+        })
+    .then((response) => {
         return response.json()
     })
 
